6de43f4e92b7306238e5447eade10def0cfae6bb
MurilloMMartins <murillomartins19@gmail.com>
2023-07-15 10:23:01 -0300

    Changed filestructure to represent modules

---
+++ modulo1/client-main.cpp
@@ -0,0 +1,74 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 5002
+#define SERVER_IP "127.0.0.1"
+
+#define MAX_MSG_SIZE 16
+
+int main(){
+    // creating socket and verifying
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(clientSocket < 0){
+        printf("Socket creation failed!\n");
+        exit(1);
+    }
+    // there's no need to bind the socket to a port
+    // the OS will automatically do it
+
+    // setting up server IP and PORT
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(SERVER_PORT);
+    // converting IP from string to bytes
+    if (inet_pton(AF_INET, SERVER_IP, &(serverAddress.sin_addr)) <= 0) {
+        printf("Invalid server address!\n");
+        exit(1);
+    }
+
+    // connect the client socket to server address
+    if(connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0){
+        printf("Couldn't connect to server!\n");
+        exit(0);
+    }
+    printf("Connection with server stabilished\n");
+    
+    // receive and send messages
+    bool running = true;
+    std::string input_msg;
+    char message[MAX_MSG_SIZE+1];
+    while(running){
+        printf("Enter a message: ");
+        std::getline(std::cin, input_msg);
+
+        // we need inputSize+1 so the message sent includes '\0'
+        int inputSize = input_msg.size()+1;
+        
+        // TCP automatically fragments depending if our input_msg is greater than MAX_MSG_SIZE
+        if(send(clientSocket, input_msg.c_str(), inputSize, 0) < 0){
+            printf("Failed to send message!\n");
+            exit(1);
+        }
+
+        // receiving reply from server
+        memset(message, 0, sizeof(message));
+        if (recv(clientSocket, message, sizeof(message), 0) < 0) {
+            printf("Failed to receive reply");
+            exit(1);
+        }
+        printf("Received reply: %s\n", message);
+
+        if(strcmp(input_msg.c_str(), "exit") == 0){
+            running = false;
+        }
+    }
+
+    printf("Client connection closed.\n");
+    close(clientSocket);
+
+    return 0;
+}
---
+++ modulo1/server-main.cpp
@@ -0,0 +1,82 @@
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <unistd.h>
+
+#define PORT 5002
+#define QUEUE_SIZE 5
+#define MAX_MSG_SIZE 16
+
+int main(){
+    // creating socket and verifying
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverSocket < 0){
+        printf("Socket creation failed!\n");
+        exit(1);
+    }
+
+    // setup server IP and PORT
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    // bind socket to the given IP and verifying
+    if(bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0){
+        printf("Socket bind failed!\n");
+        exit(1);
+    }
+
+    // listen for QUEUE_SIZE connections
+    if(listen(serverSocket, QUEUE_SIZE) != 0){
+        printf("Error while trying to listen!\n");
+        exit(1);
+    }
+    printf("Server listening on port %d\n", PORT);
+
+    // accept connection from client and verifying
+    struct sockaddr_in clientAddress;
+    socklen_t clientLenght = sizeof(clientAddress);
+    int newSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientLenght);
+    if(newSocket < 0){
+        printf("Server failed to connect to client!\n");
+        exit(0);
+    }
+    printf("Connection with client stabilished.\n");
+
+    // receive and send messages
+    bool running = true;
+    char message[MAX_MSG_SIZE+1];
+    while(running){
+        // reseting message to receive new one
+        memset(message, 0, sizeof(message));
+
+        // receiving message from client
+        int received_bytes = recv(newSocket, message, MAX_MSG_SIZE, 0);
+        if(received_bytes < 0){
+            printf("Failed to receive message!\n");
+            exit(1);
+        }
+        printf("Received message: %s (%i bytes)\n", message, received_bytes);
+
+        // sending a reply message
+        char reply[] = "Server reply";
+        if(send(newSocket, reply, strlen(reply)+1, 0) < 0){
+            printf("Failed to send reply!\n");
+            exit(1);
+        }
+
+        // exiting if client exit
+        if(strcmp(message, "exit") == 0){
+            running = false;
+        }
+    }
+
+    printf("Server connection closed.\n");
+    // closing server and client socket
+    close(serverSocket);
+    close(newSocket);
+
+    return 0;
+}
