e8473012d3305ec86ed2860fa1af036464a9951d
Gustavo <gustavosb64@gmail.com>
2023-07-15 01:39:14 -0300

    client: threads to receive and send messages

--- client-main.cpp
+++ client-main.cpp
@@ -4,12 +4,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <csignal>
+#include <thread>
+#include <vector>
 #include <arpa/inet.h>
 
-#define SERVER_PORT 5002
+#define SERVER_PORT 5001
 #define SERVER_IP "127.0.0.1"
 
 #define MAX_MSG_SIZE 16
+
+int running = true;
 
 // creating socket and verifying
 int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,6 +22,48 @@ void sigpipe_handler(int err){
     printf("Server disconnected. Closing client %d.\n", clientSocket);
     close(clientSocket);
     exit(1);
+}
+
+void send_message(std::string input_msg){
+
+    while(running){
+        //printf("Enter a message: ");
+        std::getline(std::cin, input_msg);
+
+        // we need inputSize+1 so the message sent includes '\0'
+        int inputSize = input_msg.size();
+        
+        // TCP automatically fragments depending if our input_msg is greater than MAX_MSG_SIZE
+        if(send(clientSocket, input_msg.c_str(), inputSize, 0) < 0){
+            printf("Failed to send message!\n");
+            exit(1);
+        }
+
+        if(strcmp(input_msg.c_str(), "exit") == 0){
+            running = false;
+        }
+    }
+
+    return;
+}
+
+void receive_message(char *message){
+
+    while(running){
+        // receiving reply from server
+        memset(message, 0, sizeof(message));
+        int server_reply = recv(clientSocket, message, sizeof(message), 0);
+        if (server_reply <= 0) {
+            if (server_reply == 0) raise(SIGPIPE);
+            printf("Failed to receive reply");
+            exit(1);
+        }
+
+        message[server_reply]=0;
+        printf("Received reply: %s\n", message);
+    }
+
+    return;
 }
 
 int main(){
@@ -53,33 +99,26 @@ int main(){
     bool running = true;
     std::string input_msg;
     char message[MAX_MSG_SIZE+1];
+
+    std::vector<std::thread> v_threads;
+
+    std::thread th_send(send_message, input_msg);
+    std::thread th_recv(receive_message, message);
+
+    /*
     while(running){
-        printf("Enter a message: ");
-        std::getline(std::cin, input_msg);
-
-        // we need inputSize+1 so the message sent includes '\0'
-        int inputSize = input_msg.size()+1;
         
-        // TCP automatically fragments depending if our input_msg is greater than MAX_MSG_SIZE
-        if(send(clientSocket, input_msg.c_str(), inputSize, 0) < 0){
-            printf("Failed to send message!\n");
-            exit(1);
-        }
+        std::thread th_send(send_message, input_msg);
+        std::thread th_recv(receive_message, message);
 
-        // receiving reply from server
-        memset(message, 0, sizeof(message));
-        int server_reply = recv(clientSocket, message, sizeof(message), 0);
-        if (server_reply <= 0) {
-            if (server_reply == 0) raise(SIGPIPE);
-            printf("Failed to receive reply");
-            exit(1);
-        }
-        printf("Received reply: %s\n", message);
+        send_message(input_msg);
+        receive_message(message);
 
-        if(strcmp(input_msg.c_str(), "exit") == 0){
-            running = false;
-        }
+
     }
+    */
+    th_send.join();
+    th_recv.join();
 
     printf("Client connection closed.\n");
     close(clientSocket);
